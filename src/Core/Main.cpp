ef999c277e8bb663e0e85ef8e3956a524e4d470c
Mosseelight <Doctorphillip233@gmail.com>
2023-08-03 14:13:40 +0200

    Input

--- src/Core/Main.cpp
+++ src/Core/Main.cpp
@@ -13,52 +13,59 @@
 #include "../include/Core/Camera.hpp"
 #include "../include/Core/ResUtil.hpp"
 
-const int SCRWIDTH = 1920;
-const int SCRHEIGHT = 1080;
+const int SCRWIDTH = 1280;
+const int SCRHEIGHT = 720;
+bool DebugWindow = false;
 
 const std::string shaderLoc = "res/Shaders";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void Update(GLFWwindow* window);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+std::vector<float> vertices =
+{
+    -1.0f, -1.0f,  1.0f,
+    1.0f, -1.0f,  1.0f,
+    1.0f,  1.0f,  1.0f,
+    -1.0f,  1.0f,  1.0f,
+    -1.0f, -1.0f, -1.0f,
+    1.0f, -1.0f, -1.0f,
+    1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f
+};
+std::vector<unsigned int> indices =
+{
+    0, 1, 2,
+    2, 3, 0,
+    1, 5, 6,
+    6, 2, 1,
+    7, 6, 5,
+    5, 4, 7,
+    4, 0, 3,
+    3, 7, 4,
+    4, 5, 1,
+    1, 0, 4,
+    3, 2, 6,
+    6, 7, 3
+};
+
+Shader *shadercube;
+Mesh *cube;
+Camera *cam;
 
 int main()
 {
-    std::vector<float> vertices =
-    {
-        -1.0f, -1.0f,  1.0f,
-        1.0f, -1.0f,  1.0f,
-        1.0f,  1.0f,  1.0f,
-        -1.0f,  1.0f,  1.0f,
-        -1.0f, -1.0f, -1.0f,
-        1.0f, -1.0f, -1.0f,
-        1.0f,  1.0f, -1.0f,
-        -1.0f,  1.0f, -1.0f
-    };
-
-    std::vector<unsigned int> indices =
-    {
-        0, 1, 2,
-	    2, 3, 0,
-        1, 5, 6,
-        6, 2, 1,
-        7, 6, 5,
-        5, 4, 7,
-        4, 0, 3,
-        3, 7, 4,
-        4, 5, 1,
-        1, 0, 4,
-        3, 2, 6,
-        6, 7, 3
-    };
-
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "SpaceSim", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCRWIDTH, SCRHEIGHT, "SpaceSim", NULL, NULL);
     glfwMakeContextCurrent(window);
     gladLoadGL();
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetKeyCallback(window, key_callback);
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
@@ -68,31 +75,15 @@ int main()
     ImGui_ImplGlfw_InitForOpenGL(window, true);          
     ImGui_ImplOpenGL3_Init();
 
-    Shader shadercube = Shader(ReadFile(shaderLoc + "/Default.vert"), ReadFile(shaderLoc + "/Default.frag"));
-    Mesh cube = Mesh(vertices, indices, glm::vec3(0,0,0), glm::vec3(0,0,0), 1);
-    Camera cam = Camera(glm::vec3(0,0,5), glm::vec3(0.0f), glm::vec3(0,0,1), 45);
+
+
+    shadercube = new Shader(ReadFile(shaderLoc + "/Default.vert"), ReadFile(shaderLoc + "/Default.frag"));
+    cube = new Mesh(vertices, indices, glm::vec3(0,0,0), glm::vec3(0,0,0), 1);
+    cam = new Camera(glm::vec3(0,0,5), glm::vec3(0.0f), glm::vec3(0,0,1), 45);
 
     while(!glfwWindowShouldClose(window))
     {
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-        ImGui::ShowDemoWindow();
-
-        glEnable(GL_DEPTH_TEST);
-        glClearColor(0.0f, 0.54f, 0.54f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        glUseProgram(shadercube.shader);
-        shadercube.setMat4("model", cube.GetModelMat());
-        shadercube.setMat4("view", cam.GetViewMat());
-        shadercube.setMat4("proj", cam.GetProjMat(SCRWIDTH, SCRHEIGHT, 0.001f, 100.0f));
-        cube.DrawMesh();
-
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        glfwSwapBuffers(window);
-        glfwPollEvents();   
+        Update(window);
     }
 
     ImGui_ImplOpenGL3_Shutdown();
@@ -102,7 +93,47 @@ int main()
     return 0;
 }
 
+void Update(GLFWwindow* window)
+{
+    if(DebugWindow)
+    {
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        
+        
+        ImGui::ShowMetricsWindow();
+
+    }
+
+
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(0.0f, 0.54f, 0.54f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+    glUseProgram(shadercube->shader);
+    shadercube->setMat4("model", cube->GetModelMat());
+    shadercube->setMat4("view", cam->GetViewMat());
+    shadercube->setMat4("proj", cam->GetProjMat(SCRWIDTH, SCRHEIGHT, 0.001f, 100.0f));
+    cube->DrawMesh();
+
+    if(DebugWindow)
+    {
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    }
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
-}  
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if(key == GLFW_KEY_GRAVE_ACCENT && action == GLFW_PRESS)
+        DebugWindow = !DebugWindow;
+}
