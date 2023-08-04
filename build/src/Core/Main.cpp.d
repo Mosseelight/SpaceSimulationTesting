4d775fe6c731aac722eee65d70d86b11ccafa5e4
Mosseelight <Doctorphillip233@gmail.com>
2023-08-04 12:25:43 +0200

    Remove Build

--- build/src/Core/Main.cpp.d
+++
@@ -1,4 +0,0 @@
-build/src/Core/Main.cpp.o: src/Core/Main.cpp \
- src/Core/../include/glad/glad.h src/include/KHR/khrplatform.h
-src/Core/../include/glad/glad.h:
-src/include/KHR/khrplatform.h:
--- build/src/Core/Mesh.cpp.d
+++
@@ -1,3 +0,0 @@
-build/src/Core/Mesh.cpp.o: src/Core/Mesh.cpp \
- src/Core/../include/Core/Mesh.hpp
-src/Core/../include/Core/Mesh.hpp:
