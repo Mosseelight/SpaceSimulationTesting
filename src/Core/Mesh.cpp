78d2fc491ce2fc2e1cb19c2406d0cf496a02bb0e
Mosseelight <Doctorphillip233@gmail.com>
2023-08-04 15:04:45 +0200

    Transform Fix

--- src/Core/Mesh.cpp
+++ src/Core/Mesh.cpp
@@ -42,10 +42,10 @@ void Mesh::DrawMesh()
 glm::mat4 Mesh::GetModelMat()
 {
     glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, position);
     model = glm::rotate(model, rotation.x, glm::vec3(1.0f,0.0f,0.0f));
     model = glm::rotate(model, rotation.y, glm::vec3(0.0f,1.0f,0.0f));
     model = glm::rotate(model, rotation.z, glm::vec3(0.0f,0.0f,1.0f));
-    model = glm::translate(model, position);
     model = glm::scale(model, glm::vec3(scale, scale, scale));
     return model;
 }
