e16b1d61f029599e7cfeaf656b3198aa46e48012
Mosseelight <Doctorphillip233@gmail.com>
2023-08-07 16:58:36 +0200

    Chages

--- src/Core/Main.cpp
+++ src/Core/Main.cpp
@@ -32,103 +32,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void Update(GLFWwindow* window);
 void ImguiMenu();
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
-void APIENTRY GLDebugMessageCallback(GLenum source, GLenum type, GLuint id,
-                            GLenum severity, GLsizei length,
-                            const GLchar *msg, const void *data)
-{
-    char* _source;
-    char* _type;
-    char* _severity;
-
-    switch (source) {
-        case GL_DEBUG_SOURCE_API:
-        _source = "API";
-        break;
-
-        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
-        _source = "WINDOW SYSTEM";
-        break;
-
-        case GL_DEBUG_SOURCE_SHADER_COMPILER:
-        _source = "SHADER COMPILER";
-        break;
-
-        case GL_DEBUG_SOURCE_THIRD_PARTY:
-        _source = "THIRD PARTY";
-        break;
-
-        case GL_DEBUG_SOURCE_APPLICATION:
-        _source = "APPLICATION";
-        break;
-
-        case GL_DEBUG_SOURCE_OTHER:
-        _source = "UNKNOWN";
-        break;
-
-        default:
-        _source = "UNKNOWN";
-        break;
-    }
-
-    switch (type) {
-        case GL_DEBUG_TYPE_ERROR:
-        _type = "ERROR";
-        break;
-
-        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
-        _type = "DEPRECATED BEHAVIOR";
-        break;
-
-        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
-        _type = "UDEFINED BEHAVIOR";
-        break;
-
-        case GL_DEBUG_TYPE_PORTABILITY:
-        _type = "PORTABILITY";
-        break;
-
-        case GL_DEBUG_TYPE_PERFORMANCE:
-        _type = "PERFORMANCE";
-        break;
-
-        case GL_DEBUG_TYPE_OTHER:
-        _type = "OTHER";
-        break;
-
-        case GL_DEBUG_TYPE_MARKER:
-        _type = "MARKER";
-        break;
-
-        default:
-        _type = "UNKNOWN";
-        break;
-    }
-
-    switch (severity) {
-        case GL_DEBUG_SEVERITY_HIGH:
-        _severity = "HIGH";
-        break;
-
-        case GL_DEBUG_SEVERITY_MEDIUM:
-        _severity = "MEDIUM";
-        break;
-
-        case GL_DEBUG_SEVERITY_LOW:
-        _severity = "LOW";
-        break;
-
-        case GL_DEBUG_SEVERITY_NOTIFICATION:
-        _severity = "NOTIFICATION";
-        break;
-
-        default:
-        _severity = "UNKNOWN";
-        break;
-    }
-
-    printf("%d: %s of %s severity, raised from %s: %s\n",
-            id, _type, _severity, _source, msg);
-}
 
 void buildVerticesFlat();
 
@@ -147,10 +50,8 @@ int main()
     GLFWwindow* window = glfwCreateWindow(SCRWIDTH, SCRHEIGHT, "SpaceSim", NULL, NULL);
     glfwMakeContextCurrent(window);
     gladLoadGL();
-    //glEnable(GL_DEBUG_OUTPUT);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetKeyCallback(window, key_callback);
-    //glDebugMessageCallback(GLDebugMessageCallback, NULL);
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
