c8b9a0114cbe6acc1f6f183f67d38cbdb4cdbe9e
Mosseelight <Doctorphillip233@gmail.com>
2023-10-23 18:47:44 -0400

    simd fix

--- src/include/Core/Math.hpp
+++ src/include/Core/Math.hpp
@@ -3,6 +3,6 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <immintrin.h>
+#include <pmmintrin.h>
 
 glm::vec4 TransformVec4(glm::vec4 vec, glm::mat4& mat);
